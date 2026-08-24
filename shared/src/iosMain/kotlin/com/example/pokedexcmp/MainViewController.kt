60a75b07aae04d3e7069c7b58284668600ccaba3
habib <habib@zktecodev.com>
2026-08-24 16:55:25 +0530

    -> Added try catch to see the actual issue

--- shared/src/iosMain/kotlin/com/example/pokedexcmp/MainViewController.kt
+++ shared/src/iosMain/kotlin/com/example/pokedexcmp/MainViewController.kt
@@ -2,11 +2,21 @@ package com.example.pokedexcmp
 
 import androidx.compose.ui.window.ComposeUIViewController
 import com.example.pokedexcmp.core.di.initKoin
+import platform.Foundation.NSLog
+import kotlin.experimental.ExperimentalNativeApi
 
-fun MainViewController() = ComposeUIViewController {
-    App()
+fun MainViewController(): platform.UIKit.UIViewController {
+    NSLog("!!!!! MainViewController() called - console pipe check")
+    return ComposeUIViewController {
+        App()
+    }
 }
 
+@OptIn(ExperimentalNativeApi::class)
 fun doInitKoin() {
+    setUnhandledExceptionHook { throwable ->
+        NSLog("!!!!! UNCAUGHT KOTLIN EXCEPTION: ${throwable::class.simpleName}: ${throwable.message}")
+        NSLog("!!!!! STACK:\n${throwable.stackTraceToString()}")
+    }
     initKoin()
 }
